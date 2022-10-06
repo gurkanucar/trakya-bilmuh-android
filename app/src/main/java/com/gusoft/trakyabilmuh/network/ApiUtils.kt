c3804ea2e0657c9dc8bb9d66331d7220a9269195
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-10-06 20:56:52 +0300

    base url updated

--- app/src/main/java/com/gusoft/trakyabilmuh/network/ApiUtils.kt
+++ app/src/main/java/com/gusoft/trakyabilmuh/network/ApiUtils.kt
@@ -3,7 +3,7 @@ package com.gusoft.trakyabilmuh.network
 class ApiUtils {
 
     companion object {
-        private const val BASE_URL = "http://192.168.0.10:8080"
+        private const val BASE_URL = "http://192.168.0.16:8080"
 
         fun getAnnouncementsDAO(): AnnouncementDAO {
             return RetrofitClient.getClient(BASE_URL).create(AnnouncementDAO::class.java)
