55515f1d84730c46d29873e458d7b795bde06975
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-10-06 23:18:14 +0300

    adapter implemented to MessageFragment

--- app/src/main/java/com/gusoft/trakyabilmuh/MessageFragment.kt
+++ app/src/main/java/com/gusoft/trakyabilmuh/MessageFragment.kt
@@ -1,59 +1,89 @@
 package com.gusoft.trakyabilmuh
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.gusoft.trakyabilmuh.adapter.MessageAdapter
+import com.gusoft.trakyabilmuh.databinding.FragmentMessageBinding
+import com.gusoft.trakyabilmuh.model.MessageModel
+import com.gusoft.trakyabilmuh.model.MessageTypes
+import com.gusoft.trakyabilmuh.network.ApiUtils
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [MessageFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class MessageFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private var _binding: FragmentMessageBinding? = null
+    private val binding get() = _binding!!
+
+    var messageList: ArrayList<MessageModel> = arrayListOf()
+    private lateinit var adapter: MessageAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_message, container, false)
+        _binding = FragmentMessageBinding.inflate(layoutInflater)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MessageFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MessageFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val messageType: MessageTypes = arguments?.get("messageType") as MessageTypes
+        Log.i("MESSAGE FRAGMENT", "onViewCreated: $messageType")
+        fetchData(messageType)
+    }
+
+    private fun fetchData(messageType: MessageTypes) {
+        ApiUtils.getMessageDAO().getMessages(messageType.toString()).enqueue(
+            object : Callback<List<MessageModel>> {
+                override fun onResponse(
+                    call: Call<List<MessageModel>>,
+                    response: Response<List<MessageModel>>
+                ) {
+                    response.body()?.let {
+                        messageList = it as ArrayList<MessageModel>
+                    }
+                    Log.i("Announcement", messageList.toString())
+
+                    messageList.reverse()
+
+
+                    binding.apply {
+                        messagesRcView.layoutManager = LinearLayoutManager(requireContext())
+                        messagesRcView.setHasFixedSize(true)
+                        adapter = MessageAdapter(messageList)
+                        adapter.onItemClick = ::onItemClick
+                        messagesRcView.adapter = adapter
+                    }
                 }
+
+                override fun onFailure(call: Call<List<MessageModel>>, t: Throwable) {
+                    Log.e("ERROR", "onFailure: " + t.toString())
+                }
+
             }
+        )
+    }
+
+    fun onItemClick(message: MessageModel) {
+        val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(message.link))
+        startActivity(browserIntent)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
