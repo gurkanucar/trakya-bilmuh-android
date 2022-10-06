7ffb69b904a572a132dc27776eca579e02d833d6
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-10-06 21:07:47 +0300

    MessageModel updated

--- app/src/main/java/com/gusoft/trakyabilmuh/model/MessageModel.kt
+++ app/src/main/java/com/gusoft/trakyabilmuh/model/MessageModel.kt
@@ -7,4 +7,11 @@ import java.time.LocalDateTime
 
 data class MessageModel(
     @SerializedName("id") @Expose val id: Long,
-): Serializable
+    @SerializedName("content") @Expose val content: String,
+    @SerializedName("link") @Expose val link: String,
+    @SerializedName("createdDateTime") @Expose val createdDateTime: String,
+    @SerializedName("updatedDateTime") @Expose val updatedDateTime: String,
+    @SerializedName("messageType") @Expose val messageType: String,
+    @SerializedName("user") @Expose val user: UserModel,
+    var expandable: Boolean,
+) : Serializable
